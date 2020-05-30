9817db0627341d3cc9bc0705adff20edb202f8ad
anott03 <amitav_nott@ryecountryday.org>
2020-05-30 14:41:55 -0400

    reading geotif + .gitignore

--- nc_processing.py
+++ nc_processing.py
@@ -1,13 +1,7 @@
-# importing of NetCDF files
-import netCDF4
-# plotting and data visualisations
-from matplotlib import pyplot as plt
-# n-dimensional arrays
-import numpy as np
-# high level functions for working with NetCDF files
-import xarray as xr
-# add colour labels to plot
-import matplotlib.patches as mpatches
+import os
+import rasterio as rio
+import earthpy as et
 
-data = xr.open_dataset('V2018344203600.L2_JPSS1_OC.nc')
-print(data)
+with rio.open('../V2018344203600.L2_JPSS1_OC.tif') as data:
+    print(data.bounds)
+
