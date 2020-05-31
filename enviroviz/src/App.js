fbabd74cddfe98c2015e53f9c265c139adbe376f
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2020-05-31 14:55:15 -0400

    Reformatted page and refactored code.

--- enviroviz/src/App.js
+++ enviroviz/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "../node_modules/react-linechart/dist/styles.css";
 import XYFrame from "semiotic/lib/XYFrame";
-import NetworkFrame from "semiotic/lib/NetworkFrame";
-import text from "./VegetationData.json";
-import { makeStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
@@ -11,13 +8,18 @@ import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Grid from "@material-ui/core/Grid";
 
-const data = [];
+//PUT ONE IMAGE INTO THE src FILE AND IMPORT IT
+import EVIImage from "./2018_EVI.png";
+
+//IMPORT DATA HERE
+import text from "./VegetationData.json";
 
 class Circle extends Component {
   render() {
     var circleStyle = {
       padding: 5,
       margin: 5,
+      marginLeft: -30,
       display: "inline-block",
       backgroundColor: this.props.bgColor,
       borderRadius: "50%",
@@ -29,31 +31,105 @@ class Circle extends Component {
   }
 }
 
-for (var i = 0; i < 9; i++) {
-  data.push({ color: text["2016"][i]["color"], coordinates: [] });
-}
-
-for (var i = 0; i < 9; i++) {
-  for (var j = 2016; j <= 2020; j++) {
-    data[i].coordinates.push({
-      date: j.toString(),
-      frequency: text[j.toString()][i]["freq"],
-    });
+class Panel extends Component {
+  render() {
+    let colors = [];
+    for (let i = 0; i < 9; i++) {
+      colors.push(this.props.data[i]["color"]);
+    }
+    return (
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          className="summary"
+        >
+          <Typography variant="h5">{this.props.dataType} Data</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails color="grey">
+          <Grid container direction="row">
+            <Grid item>
+              <XYFrame {...this.props.frameProps} hoverAnnotation={true} />
+            </Grid>
+            <Grid item>
+              <div style={{ padding: 30 }}>
+                <Grid container direction="column">
+                  {colors.map((color) => (
+                    <Grid container direction="row">
+                      <Grid item>
+                        <Circle bgColor={color} />
+                      </Grid>
+                      <Grid item>
+                        {colors.indexOf(color) === 0 ? (
+                          <div style={{ padding: 5 }}>
+                            <text fontSize="15px">
+                              Lowest {this.props.dataType}
+                            </text>
+                          </div>
+                        ) : colors.indexOf(color) === 8 ? (
+                          <div style={{ padding: 5 }}>
+                            <text fontSize="15px">
+                              {" "}
+                              Highest {this.props.dataType}
+                            </text>
+                          </div>
+                        ) : null}
+                      </Grid>
+                    </Grid>
+                  ))}
+                </Grid>
+              </div>
+            </Grid>
+            <Grid item>
+              <div style={{ paddingTop: 40 }}>
+                <img src={this.props.image} with="350px" height="250px" />
+                <Typography align="center"> Sample EVI Image </Typography>
+              </div>
+            </Grid>
+            <Grid item>
+              <Typography>{this.props.description}</Typography>
+            </Grid>
+          </Grid>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    );
   }
 }
 
-console.log(data);
+function processData(data) {
+  var processedData = [];
+  for (var i = 0; i < 9; i++) {
+    processedData.push({ color: data["2016"][i]["color"], coordinates: [] });
+  }
 
-const frameProps = {
-  lines: data,
+  for (var i = 0; i < 9; i++) {
+    for (var j = 2016; j <= 2020; j++) {
+      processedData[i].coordinates.push({
+        date: j.toString(),
+        frequency: data[j.toString()][i]["freq"],
+      });
+    }
+  }
+  return processedData;
+}
+
+//put data in the form [{color: "some rgb value", coordinates: [{year: "2016", frequency: "some frequency"}, ...]}...]
+//if your JSON file is in the same format as mine, the processData method should work for you too. Just pass in the text from the JSON file
+var EVIdata = processData(text);
+var Albedodata; //fill this out @Deepta
+var SSTdata; //fill this out @Amitav
+
+const EVIframeProps = {
+  lines: EVIdata,
   lineType: "linepercent",
-  size: [800, 400],
+  size: [700, 400],
   margin: { left: 100, bottom: 90, right: 30, top: 40 },
   xAccessor: "date",
   yAccessor: "frequency",
   title: <text> EVI in May of 2016-2020 </text>,
   lineStyle: (d, i) => ({
-    stroke: data[i]["color"],
+    stroke: EVIdata[i]["color"],
     strokeWidth: 3,
     fill: "none",
   }),
@@ -77,7 +153,90 @@ const frameProps = {
       tickValues: [2016, 2017, 2018, 2019, 2020],
     },
   ],
-  hoverAnnotation: true,
+
+  tooltipContent: (d) => (
+    <div className="tooltip-content">
+      <p>Pixels: {d.frequency}</p>
+    </div>
+  ),
+};
+
+//@Deepta change the "lines" to your data, change title, margins
+const AlbedoframeProps = {
+  lines: EVIdata,
+  lineType: "linepercent",
+  size: [700, 400],
+  margin: { left: 100, bottom: 90, right: 30, top: 40 },
+  xAccessor: "date",
+  yAccessor: "frequency",
+  title: <text> EVI in May of 2016-2020 </text>,
+  lineStyle: (d, i) => ({
+    stroke: EVIdata[i]["color"],
+    strokeWidth: 3,
+    fill: "none",
+  }),
+
+  title: (
+    <text textAnchor="middle" fontSize="25px">
+      EVI in the Month of May
+    </text>
+  ),
+  axes: [
+    {
+      orient: "left",
+      label: {
+        name: "Percentage of Pixels of a Given Color",
+        locationDistance: 60,
+      },
+    },
+    {
+      orient: "bottom",
+      label: { name: "Year", locationDistance: 55 },
+      tickValues: [2016, 2017, 2018, 2019, 2020],
+    },
+  ],
+
+  tooltipContent: (d) => (
+    <div className="tooltip-content">
+      <p>Pixels: {d.frequency}</p>
+    </div>
+  ),
+};
+
+//@Amitav change the "lines" to your data, change title, margins
+const SSTframeProps = {
+  lines: EVIdata,
+  lineType: "linepercent",
+  size: [700, 400],
+  margin: { left: 100, bottom: 90, right: 30, top: 40 },
+  xAccessor: "date",
+  yAccessor: "frequency",
+  title: <text> EVI in May of 2016-2020 </text>,
+  lineStyle: (d, i) => ({
+    stroke: EVIdata[i]["color"],
+    strokeWidth: 3,
+    fill: "none",
+  }),
+
+  title: (
+    <text textAnchor="middle" fontSize="25px">
+      EVI in the Month of May
+    </text>
+  ),
+  axes: [
+    {
+      orient: "left",
+      label: {
+        name: "Percentage of Pixels of a Given Color",
+        locationDistance: 60,
+      },
+    },
+    {
+      orient: "bottom",
+      label: { name: "Year", locationDistance: 55 },
+      tickValues: [2016, 2017, 2018, 2019, 2020],
+    },
+  ],
 
   tooltipContent: (d) => (
     <div className="tooltip-content">
@@ -88,42 +247,41 @@ const frameProps = {
 
 export default class App extends Component {
   render() {
-    let colors = [];
-    for (let i = 0; i < 9; i++) {
-      colors.push(data[i]["color"]);
-    }
-
     return (
-      <div>
-        <div className="App">
-          <ExpansionPanel>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-              className="summary"
-            >
-              <Typography>EVI Data</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails color="grey">
-              <Grid container direction="row">
-                <Grid item>
-                  <XYFrame {...frameProps} tooltipStyles />
-                </Grid>
-                <Grid item>
-                  <div style={{ padding: 30 }}>
-                    <Grid container direction="column">
-                      {colors.map((color) => (
-                        <Grid item>
-                          <Circle bgColor={color} />
-                        </Grid>
-                      ))}
-                    </Grid>
-                  </div>
-                </Grid>
-              </Grid>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+      <div style={{ padding: 30 }}>
+        <Typography color="textPrimary" variant="h3" align="center">
+          Environmental Impact of COVID-19
+        </Typography>
+        <div style={{ padding: 30 }}>
+          <Panel
+            frameProps={EVIframeProps}
+            data={EVIdata}
+            image={EVIImage}
+            dataType={"EVI"}
+            description={
+              "EVI, or Enhanced vegetation index is a measure of vegetation greenness. Regions with a higher EVI value have more and healthier vegetation compared to regions with a lower EVI value. The graph on the right was created by using NASA MODIS HDF data to generate an image of the Long Island region and then counting the number of pixels at each EVI threshold. The graph represents the change in the EVI of Long Island during the month of May over time from 2016 to 2020."
+            }
+          />
+          {
+            //@Deepta change the data to Albedodata and add an image + description
+          }
+          <Panel
+            frameProps={EVIframeProps}
+            data={EVIdata}
+            image={EVIImage}
+            dataType={"Albedo"}
+            description={"Add description here"}
+          />
+          {
+            //@Amitav change the data to SSTdata and change image + description
+          }
+          <Panel
+            frameProps={EVIframeProps}
+            data={EVIdata}
+            image={EVIImage}
+            dataType={"SST"}
+            description={"Add description here"}
+          />
         </div>
       </div>
     );
