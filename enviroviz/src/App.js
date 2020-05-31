191f8c71e2f20ce7463b139b1e5f4d1cec0847db
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2020-05-30 20:39:20 -0400

    Created graph template

--- enviroviz/src/App.js
+++ enviroviz/src/App.js
@@ -1,65 +1,71 @@
-var React = require('react');
-var Component = React.Component;
-var CanvasJSReact = require('./canvasjs.react');
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class App extends Component {
-	render() {
-		const options = {
-			animationEnabled: true,
-			exportEnabled: true,
-			theme: "light2", // "light1", "dark1", "dark2"
-			title:{
-				text: "Bounce Rate by Week of Year"
-			},
-			axisY: {
-				title: "Bounce Rate",
-				includeZero: false,
-				suffix: "%"
-			},
-			axisX: {
-				title: "Week of Year",
-				prefix: "W",
-				interval: 2
-			},
-			data: [{
-				type: "line",
-				toolTipContent: "Week {x}: {y}%",
-				dataPoints: [
-					{ x: 1, y: 64 },
-					{ x: 2, y: 61 },
-					{ x: 3, y: 64 },
-					{ x: 4, y: 62 },
-					{ x: 5, y: 64 },
-					{ x: 6, y: 60 },
-					{ x: 7, y: 58 },
-					{ x: 8, y: 59 },
-					{ x: 9, y: 53 },
-					{ x: 10, y: 54 },
-					{ x: 11, y: 61 },
-					{ x: 12, y: 60 },
-					{ x: 13, y: 55 },
-					{ x: 14, y: 60 },
-					{ x: 15, y: 56 },
-					{ x: 16, y: 60 },
-					{ x: 17, y: 59.5 },
-					{ x: 18, y: 63 },
-					{ x: 19, y: 58 },
-					{ x: 20, y: 54 },
-					{ x: 21, y: 59 },
-					{ x: 22, y: 64 },
-					{ x: 23, y: 59 }
-				]
-			}]
-		}
-		return (
-		<div>
-			<CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+import React, { Component } from "react";
+import "../node_modules/react-linechart/dist/styles.css";
+import XYFrame from "semiotic/lib/XYFrame";
+import NetworkFrame from "semiotic/lib/NetworkFrame";
+
+const data = [
+  {
+    color: "1",
+    coordinates: [
+      { date: 2016, frequency: 50 },
+      { date: 2017, frequency: 54 },
+      { date: 2018, frequency: 100 },
+    ],
+  },
+];
+const theme = [
+  "#ac58e5",
+  "#E0488B",
+  "#9fd0cb",
+  "#e0d33a",
+  "#7566ff",
+  "#533f82",
+  "#7a255d",
+  "#365350",
+  "#a19a11",
+  "#3f4482",
+];
+const frameProps = {
+  lines: data,
+  size: [700, 400],
+  margin: { left: 80, bottom: 90, right: 10, top: 40 },
+  xAccessor: "date",
+  yAccessor: "frequency",
+  lineStyle: (d, i) => ({
+    stroke: theme[i],
+    strokeWidth: 4,
+    fill: "none",
+  }),
+  title: <text textAnchor="middle"></text>,
+  axes: [
+    {
+      orient: "left",
+      label: "Number of Theaters",
+    },
+    {
+      orient: "bottom",
+      label: { name: "Weeks from Opening Day", locationDistance: 55 },
+      tickValues: [2016, 2017, 2018],
+    },
+  ],
+  hoverAnnotation: true,
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <XYFrame
+            {...frameProps}
+            tooltipContent={(d) => (
+              <div className="tooltip-content">
+                <p>Name: {d.frequency}</p>
+              </div>
+            )}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-module.exports = App;
