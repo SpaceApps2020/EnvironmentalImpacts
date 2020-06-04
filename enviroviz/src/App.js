89a7ef1901cf9ad145c88232d68512ee5e187bb6
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2020-06-03 22:35:08 -0400

    Cleaned up files and removed unnecessary dependencies

--- enviroviz/src/App.js
+++ enviroviz/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import "../node_modules/react-linechart/dist/styles.css";
 import XYFrame from "semiotic/lib/XYFrame";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 
